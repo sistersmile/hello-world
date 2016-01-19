de59a3d9fac9672c67ab53284c4b3079ca5acb71
alex <alex@sistersmile.com>
2016-01-19 15:02:10 +0000

    public function

--- test/app/appTest.php
+++ test/app/appTest.php
@@ -1,7 +1,9 @@
 <?php
 class appTest extends \PHPUnit_Framework_TestCase{
-  $app = new app();
-  $app->getPropertyOrSomething();
-  $this->assertEquals($app->getPropertyOrSomething(), 'Watchalookingfor?');
+  public function appTest(){    
+    $app = new app();
+    $app->getPropertyOrSomething();
+    $this->assertEquals($app->getPropertyOrSomething(), 'Watchalookingfor?');
+  }
 }
  ?>
